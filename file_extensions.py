eb97ef59ff03c5f9b89765032579cb37095198de
Thien Le <thienle.nc@gmail.com>
2020-02-27 21:59:31 -0500

    Able to orginize most of the type of files in the right bins

--- file_extensions.py
+++ file_extensions.py
@@ -1,9 +1,9 @@
 def getExtDict():
     main_EXT_dict = {}
-    main_EXT_dict[ "AUDIO"] = ["aif", ".cda", "mid", "mp3", "mpa", "ogg", 'wav', "wma", "wpl"]
-    main_EXT_dict[ "VIDEO"] = [".mp4", ".m4a", ".m4v", "f4v", ".f4a", ".m4b", "m4r", ".mov", ".avi", "wmv", ".flv"]
-    main_EXT_dict[ "IMAGES"] = [".jpeg", ".png", ".svg", "gif", ".bmp"]
-    main_EXT_dict[ "ADOBE"] = [".psd", ".ae", ".pr"]
-    main_EXT_dict[ "DOCUMENTS"] = [".pdf", ".doc", ".txt", ",docx", ".html"]
-    main_EXT_dict[ "DATA"] = [".csv", ".xlsx"]
+    main_EXT_dict["AUDIO"] = [".aif", ".cda", ".mid", ".mp3", ".mpa", ".ogg", '.wav', ".wma", ".wpl"]
+    main_EXT_dict["VIDEO"] = [".mp4", ".m4a", ".m4v", ".f4v", ".f4a", ".m4b", ".m4r", ".mov", ".avi", ".wmv", ".flv"]
+    main_EXT_dict["IMAGES"] = [".jpeg", ".png", ".svg", ".gif", ".bmp",".jpg"]
+    main_EXT_dict["ADOBE"] = [".psd", ".ae", ".pr"]
+    main_EXT_dict["DOCUMENTS"] = [".pdf", ".doc", ".txt", ".docx", ".html",".pptx", ".ppt"]
+    main_EXT_dict["DATA"] = [".csv", ".xlsx"]
     return main_EXT_dict
--- main.py
+++ main.py
@@ -5,21 +5,23 @@ from datetime import datetime,date
 from file_extensions import getExtDict
 
 class Cleaner:
-    def __init__(self, path, dir_folders, dict_ext):
+    def __init__(self, path, dir_folders, dict_ext, recent_date):
         self.path = path
         self.files = []
         self.dir_folders = dir_folders
         self.dict_ext = dict_ext
+        self.recent_date = recent_date
+        self.currentTime = datetime.now()
 
         self.dirMaker()
         self.CreateFolder()
         self.folderOrginizer()
+        duration = self.currentTime - datetime.now()
+        print("This Program took about ", duration.seconds)
 
     def dirMaker(self):
         os.chdir(self.path)
         self.files = os.listdir()
-        # Clean out duplicates
-        [os.remove(x) for x in self.files if re.search("\(\d{1}\)", x)]
         
     def CreateFolder(self):
         for dir in self.dir_folders:
@@ -27,33 +29,43 @@ class Cleaner:
                 os.mkdir("./{}".format(dir))
         print("Directories have been created successfuly.")
     
-    def getTimeDiff(self):
-        for f in self.files:
-            currentTime = datetime.now()
-            mtime = os.path.getmtime(f)
-            file_time = datetime.fromtimestamp(mtime)
-            diff = currentTime - file_time
-            return diff.days
+    def getTimeDiff(self,f):
+        self.currentTime
+        mtime = os.path.getmtime(f)
+        file_time = datetime.fromtimestamp(mtime)
+        diff = self.currentTime - file_time
+        return diff.days
 
+    # TODO: Set a flag if the user want their duplicate files to be remove
     def folderOrginizer(self):
         dict_ext = self.dict_ext
         for f in self.files:
-            name, extension = os.path.splitext(f)
-            for key,val in dict_ext.items():
-                if extension in val:
-                    shutil.move(f, "./{}/{}".format(key,f))   
+            if re.search("\(\d+\)", f):
+                print(f)
+                if os.path.isdir(f):
+                    shutil.rmtree(f)
                 else:
-                    if name not in self.dir_folders:
-                        if os.path.isdir(name):
-                            shutil.move(f, "./FOLDERS/{}".format(f))
-                        elif self.getTimeDiff() < 5:
-                            print("Test",self.getTimeDiff())
-                            shutil.move(f, "./RECENT/{}".format(f))
-                        else:
-                            shutil.move(f,"./OTHERS/{}".format(f))
+                    os.remove(f) 
+            else:
+                file_birth = self.getTimeDiff(f)
+                moved_file_flag = False
+                name, extension = os.path.splitext(f)
+                for key,val in dict_ext.items():
+                    if str.lower(extension) in val:
+                        shutil.move(f, "./{}/{}".format(key,f)) 
+                        moved_file_flag = True
+                if name not in self.dir_folders and moved_file_flag == False:
+                    if os.path.isdir(name):
+                        shutil.move(f, "./FOLDERS/{}".format(f))
+                    elif file_birth < self.recent_date:
+                        shutil.move(f, "./RECENT/{}".format(f))
+                    else:
+                        shutil.move(f,"./OTHERS/{}".format(f))
 
+# TODO: Build an unpacker, allow the user to unpack files from all or certain folders in case they want to add more folders later on.
 path = "/Users/thienle/Desktop/MainDesktop/DataScience/Python/FolderOrganizerAutomation/TestFolder"
 dict_ext = getExtDict()
 DIRS = list(dict_ext.keys()) + ["FOLDERS", "OTHERS","RECENT"]
+recent_date = 2
 if __name__ == "__main__":
-    folders_cleaner = Cleaner(path, DIRS,dict_ext)
+    folders_cleaner = Cleaner(path, DIRS,dict_ext, recent_date)
